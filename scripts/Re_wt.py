8c92f6814375355a7082eb7c663d37a31bd531a3
BenoitBalzac <38462985+BenoitBalzac@users.noreply.github.com>
2019-08-18 08:52:19 -0500

    Update Re_wt.py

--- scripts/Re_wt.py
+++ scripts/Re_wt.py
@@ -5,19 +5,23 @@ if __name__=='__main__':
 
    count_me = int(raw_input("How many atoms are there:"))
 
+   bands=int(raw_input("How many Bloch bands did you use:"))
+
    trig1='band No.'
-   bands= 60
+#   bands= 60
    dof = 1
 
    xwt=0.
    ywt=0.
    zwt=0.
-   
+
+
+   strang=""
 
    begin=10**10
    kpt=1
 #   holder=None
-   
+
    if os.path.exists('OUTCAR'):
       lines=open('OUTCAR','r').readlines()[1:]
       for i, line in enumerate(lines):
@@ -29,30 +33,24 @@ if __name__=='__main__':
                tmp_nome=str('deltEps_%s.deig' %str(dof))
                cmd="awk '{print $1, $2, $3*(%f)}' " %(xwt)+tmp_nome+" > deltEps_%s_primo.deig" %str(dof)
                tmp_nome=str('deltEps_%s.deig' %str(dof))
-               print os.popen(cmd).read()
-               print(xwt) 
+               print(os.popen(cmd).read())
+               print(xwt)
                dof+=1
                ywt=float(lines[el].split()[4])
                tmp_nome=str('deltEps_%s.deig' %str(dof))
                cmd="awk '{print $1, $2, $3*(%f)}' " %(ywt)+tmp_nome+" > deltEps_%s_primo.deig" %str(dof)
-               print os.popen(cmd).read()
+               print(os.popen(cmd).read())
                dof+=1
                print(ywt)
                zwt=float(lines[el].split()[5])
                tmp_nome=str('deltEps_%s.deig' %str(dof))
                cmd="awk '{print $1, $2, $3*(%f)}' " %(zwt)+tmp_nome+" > deltEps_%s_primo.deig" %str(dof)
-               print os.popen(cmd).read()
+               print(os.popen(cmd).read())
                dof+=1
                print(zwt)
-#  
-#            if flag>0: 
-#               Dells=open('deltEps_%s.deig' %(str(flag)),'w')
-#         if i>begin:
-#            if trig1 in lines[i]:
-#               for j in range(i+1,i+1+bands):
-##                 holder=str( lines[j])
-#                  bandno=int(lines[j].split()[0])
-#                  celta= float(lines[j].split()[1])
-#                 
-#                  Dells.write("%d        %d        %.6f\n" %(bandno,kpt,celta))
-#               kpt+=1
+   for jj in range(1,3*count_me):
+      strang+=" deltEps_%s_primo.deig" %str(jj)
+#   strang+="deltEps_%s_primo.deig" %str(3*count_me)
+
+   cmd="awk '{a[FNR]=$1;b[FNR]=$2;c[FNR]+=$3} END{for (i=1; i<=FNR; i++) print a[i],b[i],c[i]}'>wannier.deig"+strang
+   print(os.popen(cmd).read())
