02f8c3f17305ee1911dd4cdf4a4096e208869d27
Uthpala Herath <ukh0001@mix.wvu.edu>
2020-04-24 15:14:44 -0400

    Removed plt.show() from postDMFT.py when show is false

--- bin/splash.py
+++ bin/splash.py
@@ -11,8 +11,10 @@ def welcome():
     )
     print("Version %s created on %s\n" % (version, date))
     print(
-        "Please cite: Vijay Singh, Uthpala Herath, Benny Wah, Xingyu Liao, Aldo H. Romero, Hyowon Park,.\n \
-    DMFTwDFT: An open-source code combining Dynamical Mean Field Theory with various Density Functional Theory packages,.\n \
-    arXiv:2002.00068 [cond-mat.str-el].\n"
+        "Please cite: Vijay Singh, Uthpala Herath, Benny Wah, Xingyu Liao, Aldo H. Romero, Hyowon Park,."
     )
+    print(
+        "DMFTwDFT: An open-source code combining Dynamical Mean Field Theory with various Density Functional Theory packages,."
+    )
+    print("arXiv:2002.00068 [cond-mat.str-el].")
     return
