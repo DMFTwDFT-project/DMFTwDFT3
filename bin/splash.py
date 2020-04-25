17c7cdabeadfb50c819f2ce0793c2bed1d4f064b
Uthpala Herath <ukh0001@mix.wvu.edu>
2020-04-25 09:23:41 -0400

    Updated spash.py

--- bin/splash.py
+++ bin/splash.py
@@ -11,9 +11,7 @@ def welcome():
     )
     print("Version %s created on %s\n" % (version, date))
     print(
-        "Please cite: Vijay Singh, Uthpala Herath, Benny Wah, Xingyu Liao, Aldo H. Romero, Hyowon Park,. \
-        DMFTwDFT: An open-source code combining Dynamical Mean Field Theory with various Density Functional Theory packages,. \
-        arXiv:2002.00068 [cond-mat.str-el].\n"
+        "Please cite: Vijay Singh, Uthpala Herath, Benny Wah, Xingyu Liao, Aldo H. Romero, Hyowon Park,. DMFTwDFT: An open-source code combining Dynamical Mean Field Theory with various Density Functional Theory packages,. arXiv:2002.00068 [cond-mat.str-el].\n"
     )
 
     print("-------------------------------------------------------")
