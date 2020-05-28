69cd0b70c3e1b1956d3a913e2dc3e7abe33b38e4
BenoitBalzac <38462985+BenoitBalzac@users.noreply.github.com>
2020-05-28 12:03:48 -0500

    Update co_eff_phon.py

--- scripts/co_eff_phon.py
+++ scripts/co_eff_phon.py
@@ -1,6 +1,33 @@
 import os, time, subprocess
 
 if __name__=='__main__':
+
+   trigger='Degree'
+   trig1='band No.'
+   bands= int(input('how many bands do you have?'))
+   flag = -1
+   begin=10**6
+   kpt=1
+
+   if os.path.exists('OUTCAR'):
+      lines=open('OUTCAR','r').readlines()[1:]
+      for i, line in enumerate(lines):
+         if trigger in lines[i]:
+            print('hello darkness my old friend')
+            begin=i
+            flag+=1
+            kpt=1
+            if flag>0:
+               Dells=open('deltEps_%s.deig' %(str(flag)),'w')
+         if i>begin:
+            if trig1 in lines[i]:
+               for j in range(i+1,i+1+bands):
+                  bandno=int(lines[j].split()[0])
+                  celta= float(lines[j].split()[1])
+                  Dells.write("%d       %d      %.6f    \n" %(bandno,kpt,celta))
+               kpt=kpt+1
+
+   
    trigger='THz'
    trig1='dx'
    num_at= int(input('how many atoms do you have?'))
@@ -33,4 +60,6 @@ if __name__=='__main__':
                count+=1
             cmd='paste Ramode_'+str(flag)+'_{1,2,3,4,5}.deig | awk \'{print $1,$2,$3+$6+$9+$12+$15}\'>Ramode_'+str(flag)+'.deig'
             print os.popen(cmd).read()
+      cmd='rm Ramode_*_*.deig | rm deltEps_*.deig'
+      print os.popen(cmd).read()
 
